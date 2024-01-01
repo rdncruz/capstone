49c31ba797f572b8ae1f045518aef7a39c631449
D-Slinger <crenzxdaryl@gmail.com>
2024-01-01 18:47:13 +0800

    Update store.php

--- store.php
+++ store.php
@@ -18,6 +18,7 @@
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    
     <link rel="stylesheet" href="css/shopping.css">
     
 </head>
