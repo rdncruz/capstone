72e36b0b0490f4ea46570f5c08ff456b0b34ea7d
D-Slinger <crenzxdaryl@gmail.com>
2024-01-02 22:44:01 +0800

    Update product_details.php

--- product_details.php
+++ product_details.php
@@ -333,10 +333,14 @@ if(isset($_GET['product_id'])) {
         </main>
     </section>
     <script src="javascript/design.js"></script>
-    <script src="javascript/product.js"></script>
+    <script src="javascript/product_details.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="lib/easing/easing.min.js"></script>
     <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 </body>
 </html>
+                    
+
+
+
