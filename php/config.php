31faf5ae03e10b686be000c370f7cf12492b04a6
D-Slinger <crenzxdaryl@gmail.com>
2024-01-05 06:38:07 +0800

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "";
+  
+  $username = "efishing";
+  $password = "PcFzHMSQPDzKgd/e";
   $dbname = "efishing";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
