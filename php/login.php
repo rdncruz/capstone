f97f874cb65617a00561b8c739a555fd77884eb1
D-Slinger <crenzxdaryl@gmail.com>
2024-01-16 12:29:10 +0800

    Update login.php

--- php/login.php
+++ php/login.php
@@ -52,11 +52,11 @@
                     $status = "Active now";
                     if ($row['role'] === 'seller' && $row['verification_status'] === 'Not Verified') {
                         echo '<script>
-                                swal("You\'ve been registered!", "Please Wait for the admin to send the OTP Verification", "success")
-                                    .then(() => {
-                                        window.location.href = "./seller_login.php";
-                                    });
-                            </script>';
+                        swal("You\'ve been registered!", "Please wait for the admin to send the OTP Verification", "error")
+                            .then(() => {
+                                window.location.href = "./seller_login.php";
+                            });
+                      </script>';
                     }
                     $sql2 = mysqli_query($conn, "UPDATE users SET otp = '{$otp}', status = '{$status}' WHERE unique_id = {$row['unique_id']}");
                     if($sql2){
